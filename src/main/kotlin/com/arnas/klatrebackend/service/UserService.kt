7fb53c2eaf38d1b8ceac855cb6c919e9ec838df8
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-07-22 14:04:59 +0200

    Minor refactoring

--- src/main/kotlin/com/arnas/klatrebackend/service/UserService.kt
+++ src/main/kotlin/com/arnas/klatrebackend/service/UserService.kt
@@ -3,7 +3,6 @@ package com.arnas.klatrebackend.service
 import com.arnas.klatrebackend.dataclass.User
 import com.arnas.klatrebackend.repository.UserRepository
 import org.json.JSONObject
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.net.URI
 import java.net.http.HttpClient
@@ -11,20 +10,33 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
 @Service
-class UserService {
+class UserService(
+    var userRepository: UserRepository
+) {
 
-    val client: HttpClient = HttpClient.newBuilder().build()
-    @Autowired
-    lateinit var userRepository: UserRepository
+    open val client: HttpClient = HttpClient.newBuilder().build()
 
-    fun getGoogleUserProfile(token: String): Map<String, String>? {
+
+    open fun registerUser(accessToken: String) {
+
+        val userInfo = getGoogleUserProfile(accessToken)
+        val email = userInfo["email"] ?: return
+        val name = userInfo["name"] ?: return
+        userRepository.createUser(User(email, name))
+    }
+
+    open fun loginUser(token: String): User? {
+        return getUserByToken(token)
+    }
+
+    open fun getGoogleUserProfile(token: String): Map<String, String> {
         val request = HttpRequest.newBuilder()
             .uri(URI.create("https://www.googleapis.com/oauth2/v1/userinfo?access_token=${token}"))
             .header("Authorization", "Bearer $token")
             .header("Accept", "application/json")
             .build()
         val response = client.send(request, HttpResponse.BodyHandlers.ofString())
-        if (response.statusCode() != 200) {return null}
+        if (response.statusCode() != 200) {return mapOf()}
         val json = JSONObject(response.body())
         return mapOf(
             "name" to json.getString("name"),
@@ -32,9 +44,9 @@ class UserService {
         )
     }
 
-    fun getUserByToken(token: String): User? {
+    open fun getUserByToken(token: String): User? {
         val userInfo = getGoogleUserProfile(token)
-        if (userInfo.isNullOrEmpty()) {return null}
+        if (userInfo.isEmpty()) {return null}
         val email = userInfo["email"] ?: return null
         val name = userInfo["name"] ?: return null
         val user: User = User(email, name)
@@ -42,7 +54,7 @@ class UserService {
         return user
     }
 
-    fun getUserID(accessToken: String): Int? {
+    open fun getUserID(accessToken: String): Int? {
         val user: User = getUserByToken(accessToken) ?: return null
         val userID: Int = userRepository.getUserIDByObject(user)
         return userID
