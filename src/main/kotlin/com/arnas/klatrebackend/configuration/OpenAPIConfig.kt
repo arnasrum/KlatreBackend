8227d12c1a9f228db9c64bc2d0cc1db54d5128ce
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-08-08 02:40:14 +0200

    Swagger setup

---
+++ src/main/kotlin/com/arnas/klatrebackend/configuration/OpenAPIConfig.kt
@@ -0,0 +1,21 @@
+package com.arnas.klatrebackend.configuration
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfig {
+
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("KlatreBook API")
+                    .version("1.0")
+                    .description("API documentation for KlatreBook backend")
+            )
+    }
+}
