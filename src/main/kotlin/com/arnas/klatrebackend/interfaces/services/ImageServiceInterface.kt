52c14fafa74ff34ae07e46293f0c4a178955aaca
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-09-17 17:26:52 +0200

    Added an interface for image service

---
+++ src/main/kotlin/com/arnas/klatrebackend/interfaces/services/ImageServiceInterface.kt
@@ -0,0 +1,15 @@
+package com.arnas.klatrebackend.interfaces.services
+
+import com.arnas.klatrebackend.dataclass.Image
+import com.arnas.klatrebackend.dataclass.ServiceResult
+import org.springframework.web.multipart.MultipartFile
+
+interface ImageServiceInterface {
+
+    fun storeImageMetadata(userId: Long, boulderId: Long, contentType: String, size: Long): ServiceResult<String>
+    fun getImageMetadata(imageId: String): ServiceResult<Image?>
+    fun getImageMetadataByBoulder(boulderId: Long): ServiceResult<Image?>
+    fun storeImageFile(file: MultipartFile, boulderId: Long,  userId: Long): ServiceResult<String>
+    fun getImage(boulderId: Long): Image?
+    fun deleteImage(image: Image)
+}
--- src/main/kotlin/com/arnas/klatrebackend/service/ImageService.kt
+++ src/main/kotlin/com/arnas/klatrebackend/service/ImageService.kt
@@ -2,19 +2,12 @@ package com.arnas.klatrebackend.service
 
 import com.arnas.klatrebackend.dataclass.Image
 import com.arnas.klatrebackend.dataclass.ServiceResult
-import com.arnas.klatrebackend.dataclass.User
+import com.arnas.klatrebackend.interfaces.services.ImageServiceInterface
 import com.arnas.klatrebackend.repository.ImageRepository
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
-import java.awt.image.BufferedImage
-import java.io.ByteArrayInputStream
 import java.io.File
-import java.nio.file.Files
-import java.util.*
-import javax.imageio.ImageIO
 
 import kotlin.io.path.Path
 import kotlin.io.path.createDirectories
@@ -23,7 +16,7 @@ import kotlin.io.path.exists
 @Service
 class ImageService(
     val imageRepository: ImageRepository,
-) {
+): ImageServiceInterface {
 
     @Value("\${app.image.upload.dir}")
     private lateinit var uploadDir: String
@@ -42,28 +35,23 @@ class ImageService(
         }
     }
 
-    open fun storeImageMetadata(boulderID: Long,
-                                contentType: String,
-                                size: Long,
-                                userID: Long
-    ): ServiceResult<String> {
-        val id = imageRepository.storeImageMetaData(boulderID, contentType, size, userID)
+    override fun storeImageMetadata(userId: Long, boulderId: Long, contentType: String, size: Long): ServiceResult<String> {
+        val id = imageRepository.storeImageMetaData(boulderId, contentType, size, userId)
         return ServiceResult(data = id, message = "Image metadata stored successfully", success = true)
     }
 
-    open fun getImageMetadata(imageId: String): ServiceResult<Image?> {
+    override fun getImageMetadata(imageId: String): ServiceResult<Image?> {
         val image = imageRepository.getImageMetadata(imageId)
         image?: return ServiceResult(data = null, message = "Image not found", success = false)
         return ServiceResult(data = image, message = "Image metadata retrieved successfully", success = true)
     }
 
-    open fun getImageMetadataByBoulder(boulderId: Long): ServiceResult<Image?> {
+    override fun getImageMetadataByBoulder(boulderId: Long): ServiceResult<Image?> {
         val image = imageRepository.getImageMetadataByBoulder(boulderId) ?: return ServiceResult(data = null, message = "Image not found", success = false)
         return ServiceResult(data = image, message = "Image metadata retrieved successfully", success = true)
     }
 
-
-    open fun storeImageFile(file: MultipartFile, boulderId: Long, userId: Long): ServiceResult<String> {
+    override fun storeImageFile(file: MultipartFile, boulderId: Long, userId: Long): ServiceResult<String> {
         try {
             init()
             if (file.isEmpty) {
@@ -75,10 +63,10 @@ class ImageService(
             }
 
             val imageId = storeImageMetadata(
-                boulderID = boulderId,
+                boulderId = boulderId,
                 contentType = file.contentType!!,
                 size = file.size,
-                userID = userId).data
+                userId = userId).data
 
             imageId?: throw Exception("Error storing image metadata")
             val filePath = File("$uploadDir/$imageId")
@@ -93,12 +81,11 @@ class ImageService(
 
     }
 
-
-    fun getImage(boulderId: Long): Image? {
+    override fun getImage(boulderId: Long): Image? {
        return imageRepository.getImageByBoulderId(boulderId)
     }
 
-    open fun deleteImage(image: Image) {
+    override fun deleteImage(image: Image) {
         imageRepository.deleteImage(image.id)
     }
 
