c52f1f64bec524f29a20dc92d8fc834644dd0800
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-10-07 19:42:12 +0200

    Removed an old function

--- src/main/kotlin/com/arnas/klatrebackend/interfaces/services/RouteSendServiceInterface.kt
+++ src/main/kotlin/com/arnas/klatrebackend/interfaces/services/RouteSendServiceInterface.kt
@@ -1,13 +1,11 @@
 package com.arnas.klatrebackend.interfaces.services
 
-import com.arnas.klatrebackend.dataclasses.BoulderWithSend
 import com.arnas.klatrebackend.dataclasses.RouteSend
 import com.arnas.klatrebackend.dataclasses.RouteSendDTOUpdate
 import com.arnas.klatrebackend.dataclasses.ServiceResult
 
 interface RouteSendServiceInterface {
     fun getUserBoulderSends(userID: Long, boulderIDs: List<Long>): ServiceResult<List<RouteSend>>
-    fun getBouldersWithSendsByPlace(userID: Long, placeID: Long): ServiceResult<List<BoulderWithSend>>
     fun addUserRouteSend(userID: Long, boulderID: Long, additionalProps: Map<String, String> = emptyMap())
 
     fun getRouteSendByRoute(routeId: Long, userId: Long): ServiceResult<RouteSend?>
