03f6965b1abf9ca2198ba88a4abc57cc08e9cc56
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-10-20 21:34:34 +0200

    Started centralizing exception handling

---
+++ src/main/kotlin/com/arnas/klatrebackend/components/RestControllerExceptionHandler.kt
@@ -0,0 +1,24 @@
+package com.arnas.klatrebackend.components
+
+import org.springframework.data.rest.webmvc.ResourceNotFoundException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class RestControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException::class)
+    fun handleResourceNotFoundException(exception: ResourceNotFoundException): ResponseEntity<Map<String, Any>> {
+        val message = exception.message?: "Resource not found"
+        val response: ResponseEntity<Map<String, Any>> = ResponseEntity.status(404).body(mapOf("message" to message))
+        return response
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleException(exception: Exception): ResponseEntity<Map<String, Any>> {
+        val message = exception.message?: "Internal server error"
+        val response: ResponseEntity<Map<String, Any>> = ResponseEntity.status(500).body(mapOf("message" to message))
+        return response
+    }
+}
