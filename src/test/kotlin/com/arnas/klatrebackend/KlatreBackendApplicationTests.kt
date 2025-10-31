1b99feb71cd4351ba8405e03735646098a9432b2
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-10-31 12:37:27 +0100

    Disabled security for tests

--- src/test/kotlin/com/arnas/klatrebackend/KlatreBackendApplicationTests.kt
+++ src/test/kotlin/com/arnas/klatrebackend/KlatreBackendApplicationTests.kt
@@ -2,12 +2,18 @@ package com.arnas.klatrebackend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
-@SpringBootTest
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.NONE,
+    properties = [
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
+    ]
+)
+@ActiveProfiles("test")
 class KlatreBackendApplicationTests {
 
     @Test
     fun contextLoads() {
     }
-
 }
