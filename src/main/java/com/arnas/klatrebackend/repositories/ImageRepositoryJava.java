b57e98d0864101ff1db47f38263b9dd22de77f8f
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-10-31 12:42:00 +0100

    Implemented ImageRepository in java

--- src/main/java/com/arnas/klatrebackend/repositories/ImageRepositoryJava.java
+++ src/main/java/com/arnas/klatrebackend/repositories/ImageRepositoryJava.java
@@ -5,8 +5,12 @@ import com.arnas.klatrebackend.interfaces.repositories.ImageRepositoryInterface;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.util.Objects;
 
 @Primary
 @Repository
@@ -20,27 +24,48 @@ public class ImageRepositoryJava implements ImageRepositoryInterface {
 
     @Override
     public @Nullable Image getImageById(@NotNull String id) {
-        System.out.println("Getting image with id: " + id);
-        return null;
+        var sql = "SELECT * FROM images WHERE id = :id";
+        var parameters = new MapSqlParameterSource().addValue("id", id);
+        return jdbcTemplate.query(sql, parameters,
+            (rs, index) -> new Image(
+                rs.getString("id"),
+                rs.getString("contentType"),
+                rs.getLong("fileSize")
+            )
+        ).getFirst();
     }
 
     @Override
     public int deleteImage(@NotNull String id) {
-        System.out.println("Getting image with id: " + id);
-        return 0;
+        var sql = "DELETE FROM images WHERE id = :id";
+        var parameters = new MapSqlParameterSource().addValue("id", id);
+        return jdbcTemplate.update(sql, parameters);
     }
 
     @Override
     @NotNull
     public String storeImageMetaData(@NotNull String contentType, long size, long userId) {
-        System.out.println("Getting image with id: " + size);
-        return "";
+        var sql = "INSERT INTO images(id, contentType, fileSize) VALUES (:id, :contentType, :fileSize)";
+        var keyholder = new GeneratedKeyHolder();
+        var parameters = new MapSqlParameterSource()
+                .addValue("id", userId)
+                .addValue("contentType", contentType)
+                .addValue("fileSize", size);
+        jdbcTemplate.update(sql, parameters, keyholder);
+        return Objects.requireNonNull(keyholder.getKey()).toString();
     }
 
     @Override
     @Nullable
     public Image getImageMetadata(@NotNull String id) {
-        System.out.println("Getting image with id: " + id);
-        return null;
+        var sql = "SELECT * FROM images WHERE id = :id";
+        var parameters = new MapSqlParameterSource().addValue("id", id);
+        return jdbcTemplate.query(sql, parameters,
+                (rs, index) -> new Image(
+                        rs.getString("id"),
+                        rs.getString("contentType"),
+                        rs.getLong("fileSize")
+                )
+        ).getFirst();
     }
 }
