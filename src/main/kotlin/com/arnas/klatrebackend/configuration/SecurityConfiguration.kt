2fe4a10962dbffef54d86a8a137b4fb95eb7a3f6
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-10-31 18:21:36 +0100

    Renamed kotlin jwtAuthenticationFilter

--- src/main/kotlin/com/arnas/klatrebackend/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/com/arnas/klatrebackend/configuration/SecurityConfiguration.kt
@@ -1,6 +1,6 @@
 package com.arnas.klatrebackend.configuration
 
-import com.arnas.klatrebackend.components.JwtAuthenticationFilter
+import com.arnas.klatrebackend.components.JwtAuthenticationFilterDefault
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -12,7 +12,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration(
-    private val jwtAuthenticationFilter: JwtAuthenticationFilter
+    private val jwtAuthenticationFilter: JwtAuthenticationFilterDefault
 ) {
     
     @Bean
